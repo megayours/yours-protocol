3ea2c38fda99d9749ce6b25e91202f3671d1538c
Viktor Plane <viktor@planevin.se>
2024-08-24 11:40:31 +0200

    feat: add husky

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      // Add your custom rules here
+    },
+  }
+);
